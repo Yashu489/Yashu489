f21e24d15570f9c1f4109f8df3733b7b3d390ab9
Yashu489 <yashugowdayashugowda361@gmail.com>
2025-08-04 17:43:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+  alert("Thank you for showing interest! 🌍 Together, we can make a difference.");
+  console.log("Join button clicked at " + new Date().toLocaleTimeString());
+}
